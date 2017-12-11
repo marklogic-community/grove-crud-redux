a9246bc2b5b5dd07264a3e4a4d48377468768cc3
Patrick McElwee <pmcelwee@gmail.com>
2017-12-11 10:04:08 -0500

    Port document code from ml-search-redux

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Public API for the documents module
+import reducer, { selectors } from './reducers'
+import * as actions from './actions'
+import * as actionTypes from './actionTypes'
+
+export default reducer
+export {
+  selectors,
+  actions,
+  actionTypes
+}
